ae4816cf187eed4f389ae93c0aa5a64ace3b5c8e
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-11-26 10:21:27 +0800

    feat: Updated font size for ProfileSkills section

--- src/ProfileSkillsSection.js
+++ src/ProfileSkillsSection.js
@@ -3,14 +3,14 @@ export function ProfileSkillsSection() {
     <div className="ProfileSkills">
 
       <div className="ProfileSkillsColumn">
-        <div>PROFILE</div>
+        <div className="ProfileSkillsHeader">PROFILE</div>
         <div>I am an NUS Computer Science student who will be graduating in May 2021. I will be looking for employment then. If I look like a fit for your job, please feel free to reach out to me!</div>
         <div>Button to download Resume</div>
         <div>Button to Email me</div>
       </div>
 
       <div className="ProfileSkillsColumn">
-        <div>SKILLS</div>
+        <div className="ProfileSkillsHeader">SKILLS</div>
         <div>My focus areas in Computer Science were Computer Graphics and Artificial Intelligence, with a minor in Interactive Media Development. I am confident that I will be able to pick up any necessary skills or competencies quickly.</div>
         <div>I am confident in my abilities in these areas:</div>
         <ul>
