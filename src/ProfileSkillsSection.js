561dce7dace5dbceb382f08dfbb3f356d511d547
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-11-26 15:58:37 +0800

    feat: Set up Project Cards to work properly

--- src/ProfileSkillsSection.js
+++ src/ProfileSkillsSection.js
@@ -4,15 +4,16 @@ export function ProfileSkillsSection() {
 
       <div className="ProfileSkillsColumn">
         <div className="ProfileSkillsHeader">PROFILE</div>
-        <div>I am an NUS Computer Science student who will be graduating in May 2021. I will be looking for employment then. If I look like a fit for your job, please feel free to reach out to me!</div>
+        <p>I am an NUS Computer Science student who will be graduating in May 2021. During my time in NUS, I was also the President of the Games Development Group.</p>
+        <p>I will be looking forward to a job in the tech industry after I graduate. If I look like a fit for your company, please feel free to reach out to me!</p>
         <div>Button to download Resume</div>
         <div>Button to Email me</div>
       </div>
 
       <div className="ProfileSkillsColumn">
         <div className="ProfileSkillsHeader">SKILLS</div>
-        <div>My focus areas in Computer Science were Computer Graphics and Artificial Intelligence, with a minor in Interactive Media Development. I am confident that I will be able to pick up any necessary skills or competencies quickly.</div>
-        <div>I am confident in my abilities in these areas:</div>
+        <p>My focus areas in Computer Science were Computer Graphics and Artificial Intelligence, with a minor in Interactive Media Development. I am confident that I will be able to pick up any necessary skills or competencies quickly.</p>
+        <p>I am confident in my abilities in these areas:</p>
         <ul>
           <li>C#</li>
           <li>Java</li>
@@ -21,7 +22,7 @@ export function ProfileSkillsSection() {
           <li>React</li>
         </ul>
       </div>
-  
+
     </div>
   );
 }
--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -1,8 +1,37 @@
+import { ProjectCard } from "./ProjectCard.tsx";
+
 export function ProjectsSection() {
   return (
     <div className="Section">
       <header> PROJECTS </header>
-      <div>Boxkid, Farming Helper, Truck</div>
+        <ProjectCard
+          projectName={"BoxKid"}
+          projectSubtitle={"A roguelike game made in Unity 3D and C#"}
+          contributions={["A", "B", "C"]}
+          imageSource={"https://cdn.discordapp.com/attachments/703224120191483908/913653902036041790/09-57-38-pomeranian-01.jpg"}
+        />
+
+        <ProjectCard
+          projectName={"AAAAAAAAA"}
+          projectSubtitle={"I want to die"}
+          contributions={["A", "B", "C"]}
+          imageSource={"https://cdn.discordapp.com/attachments/703224120191483908/913653902036041790/09-57-38-pomeranian-01.jpg"}
+        />
+
+        <ProjectCard
+          projectName={"Truck"}
+          projectSubtitle={"A real-time strategy game made in Unity 3D and C#"}
+          contributions={["A", "B", "C"]}
+          imageSource={"https://cdn.discordapp.com/attachments/703224120191483908/913653902036041790/09-57-38-pomeranian-01.jpg"}
+        />
+
+        <ProjectCard
+          projectName={"Horo"}
+          projectSubtitle={"A scheduling tool written in Java"}
+          contributions={["A", "B", "C"]}
+          imageSource={"https://cdn.discordapp.com/attachments/703224120191483908/913653902036041790/09-57-38-pomeranian-01.jpg"}
+        />
+
     </div>
   );
 };
