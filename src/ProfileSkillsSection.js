18e10383c05f708d27833aa92a3cad65d06ba4b1
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-12-01 16:48:59 +0800

    feat: ported projects to mui

--- src/ProfileSkillsSection.js
+++ src/ProfileSkillsSection.js
@@ -1,13 +1,49 @@
 import { Box } from "@mui/system";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Stack, Typography } from "@mui/material";
 import { Article, Email } from '@mui/icons-material';
 
 export function ProfileSkillsSection() {
   return (
-    <Box id="aboutme" sx={{my: "5vh"}}>
+    <Box id="aboutme" sx={{ my: "5vh" }}>
       <Typography align="center">ABOUT ME</Typography>
 
-      <Box width="50vw" sx={{ mx: "auto", mt: "-8vh"}}>
+      <Box width="50vw" sx={{ mx: "auto" }}>
+        <Stack direction="row" spacing={4}>
+
+          <Box width="50vw">
+            <Typography paragraph={true}>I am an NUS Computer Science student who will be graduating in May 2021. During my time in NUS, I was also the President of the Games Development Group.</Typography>
+            <Typography paragraph={true}>I will be looking forward to a job in the tech industry after I graduate. If I look like a fit for your company, please feel free to reach out to me!</Typography>
+
+            <Button sx={{ my: "0.5vh" }} variant="contained" startIcon={<Article />} href="https://cdn.discordapp.com/attachments/703224120191483908/913653902036041790/09-57-38-pomeranian-01.jpg">
+              Download my resume!
+            </Button>
+
+            <Button sx={{ my: "0.5vh" }} variant="contained" startIcon={<Email />} href="mailto:chenggeng1997@gmail.com">
+              Contact me by email!
+            </Button>
+          </Box>
+
+          <Box width="50vw">
+            <Typography paragraph={true}>My focus areas in Computer Science were Computer Graphics and Artificial Intelligence, with a minor in Interactive Media Development. I am confident that I will be able to pick up any necessary skills or competencies quickly.</Typography>
+            <Typography paragraph={true}>I am confident in my abilities in these areas:</Typography>
+            <ul>
+              <li>C#</li>
+              <li>Java</li>
+              <li>C++</li>
+              <li>JavaScript</li>
+              <li>React</li>
+            </ul>
+          </Box>
+
+        </Stack>
+      </Box>
+
+    </Box>
+  );
+}
+
+
+/*
         <Grid container rowSpacing={1} alignItems="center" justify="center" margin="5vw">
           <Grid item sx={{ width: "20vw", height: "20vw" }} margin="auto">
             <Typography paragraph={true}>I am an NUS Computer Science student who will be graduating in May 2021. During my time in NUS, I was also the President of the Games Development Group.</Typography>
@@ -35,8 +71,5 @@ export function ProfileSkillsSection() {
           </Grid>
 
         </Grid>
-      </Box>
 
-    </Box>
-  );
-}
+*/
--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,39 +1,78 @@
+import { Link, YouTube, GitHub } from '@mui/icons-material';
+import { Button, Box, Paper, Stack, Typography } from '@mui/material';
+
 type ProjectCardProps = {
   projectName: string;
   imageSource: string;
   projectSubtitle: string;
   contributions: string[];
-  link: string;
+  demoLink?: string;
+  githubLink?: string;
+  youtubeLink?: string;
 };
 
 export const ProjectCard: React.FC<ProjectCardProps> = props => {
-
   return (
-    <div className="ProjectCard">
-      <picture>
-        <img
-          alt={props.projectName}
-          className="ProjectPicture"
-          src={props.imageSource}
-        >
-        </img>
-      </picture>
-      <div className="ProjectDetails">
-        <div className="ProjectTitle">{props.projectName}</div>
-        <div className="ProjectSubtitle">{props.projectSubtitle}</div>
+    <Paper sx={{ margin: "5vh 5vw", width: "80vw", height: "40vh", padding: "3vh 3vw" }} variant="elevation" elevation={4}>
+      <Stack direction="row" spacing={4}>
 
-        <div className="Contributions">
-          <div className="MyContributionsTitle">My Contributions</div>
-          <ul>
-            {props.contributions.map((contributionString: string) => {
-              return <li>{contributionString}</li>;
-            })}
-          </ul>
+        <Box>
+          <picture>
+            <img
+              alt={props.projectName}
+              src={props.imageSource}
+              className="ProjectPicture"
+            >
+            </img>
+          </picture>
+        </Box>
 
-          Check it out <a className="ProjectCardLink" href={props.link}>here</a>!
-        </div>
-      </div>
-    </div>
+        <Box>
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: 'bold' }}
+          >
+            {props.projectName}
+          </Typography>
+
+          <Typography
+            variant="subtitle1"
+            sx={{ fontStyle: 'italic' }}
+          >
+            {props.projectSubtitle}
+          </Typography>
+
+          <Box sx={{ width: 100, height: 5, paddingBottom: '20px' }} className="center-contents">
+            <Box sx={{ width: 100, height: 5, margin: 'auto' }} className="primary-background-color" />
+          </Box>
+
+          <Typography>
+            <ul>
+              {props.contributions.map((contributionString: string) => {
+                return <li>{contributionString}</li>;
+              })}
+            </ul>
+          </Typography>
+
+          {props.demoLink &&
+            <Button sx={{ mx: "0.5vh" }} variant="contained" startIcon={<Link />}>
+              Link
+            </Button>
+          }
+          {props.githubLink &&
+            <Button sx={{ mx: "0.5vh" }} variant="contained" startIcon={<GitHub />}>
+              GitHub
+            </Button>
+          }
+          {props.youtubeLink &&
+            <Button sx={{ mx: "0.5vh" }} variant="contained" startIcon={<YouTube />}>
+              Video
+            </Button>
+          }
+        </Box>
+
+      </Stack>
+    </Paper>
   );
 }
 
