f87d2cbf0cdb32b0d90f17a37424d7c2e58a0fbe
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-12-01 14:49:01 +0800

    feat: centered Navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,17 @@
-import { AppBar, Button, Container } from "@mui/material";
+import { AppBar, Box, Button, Container } from "@mui/material";
 
 export function NavBar() {
   return (
     <AppBar position="sticky" enableColorOnDark={true}>
-      <Container maxWidth="xl">
-        <Button sx={{ color: 'white' }} href="#home">HOME</Button>
-        <Button sx={{ color: 'white' }} href="#aboutme">ABOUT</Button>
-        <Button sx={{ color: 'white' }} href="#workexp">WORK EXPERIENCE</Button>
-        <Button sx={{ color: 'white' }} href="#projects">PROJECTS</Button>
-        <Button sx={{ color: 'white' }} href="#contact">CONTACT</Button>
-      </Container>
+      <Box sx={{ mx: "auto" }}>
+        <Container maxWidth="xl">
+          <Button sx={{ color: 'white' }} href="#home">HOME</Button>
+          <Button sx={{ color: 'white' }} href="#aboutme">ABOUT</Button>
+          <Button sx={{ color: 'white' }} href="#workexp">WORK EXPERIENCE</Button>
+          <Button sx={{ color: 'white' }} href="#projects">PROJECTS</Button>
+          <Button sx={{ color: 'white' }} href="#contact">CONTACT</Button>
+        </Container>
+      </Box>
     </AppBar>
   );
 }
