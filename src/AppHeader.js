921f9f7618ebf9eb16baa9a2c7e28b6b1aa0f9ed
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-12-01 17:35:09 +0800

    feat: Migrated header to mui

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,31 +1,30 @@
 import { Email, GitHub, LinkedIn } from '@mui/icons-material';
-import IconButton from '@mui/material/IconButton';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import { ButtonGroup, IconButton, Typography } from '@mui/material';
+import { Box } from "@mui/system";
 
 export function AppHeader() {
   return (
-    <header id="home" className="App-header">
-      <div className="DeveloperName"><strong>Ong Cheng Geng</strong></div>
-      <div className="DeveloperSubtitle">
-        <div>Software Developer</div>
-        <div>Background: Whale Shark by JimmyDominico</div>
-      </div>
+    <Box id="home">
+      <Typography sx={{ fontWeight: 'bold' }}>Ong Cheng Geng</Typography>
+      <Typography>Software Developer</Typography>
+      <Typography>Background Image by JimmyDominico</Typography>
 
-      <ButtonGroup>
-        <IconButton color="secondary" href="https://github.com/ChengGeng97">
-          <GitHub fontSize="large" />
-        </IconButton>
+      <Box>
+        <ButtonGroup>
+          <IconButton color="secondary" href="https://github.com/ChengGeng97">
+            <GitHub fontSize="large" />
+          </IconButton>
 
-        <IconButton color="secondary" href="https://www.linkedin.com/in/cheng-geng-ong-5414361a2/">
-          <LinkedIn fontSize="large" />
-        </IconButton>
+          <IconButton color="secondary" href="https://www.linkedin.com/in/cheng-geng-ong-5414361a2/">
+            <LinkedIn fontSize="large" />
+          </IconButton>
 
-        <IconButton color="secondary" href="mailto:chenggeng1997@gmail.com">
-          <Email fontSize="large" />
-        </IconButton>
+          <IconButton color="secondary" href="mailto:chenggeng1997@gmail.com">
+            <Email fontSize="large" />
+          </IconButton>
+        </ButtonGroup>
+      </Box>
 
-      </ButtonGroup>
-
-    </header>
+    </Box>
   );
 }
