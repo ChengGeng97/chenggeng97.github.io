c30ca665950dffb8845b278f7736c32a6c9fe209
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-12-01 14:54:50 +0800

    feat: centered footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 
 export function Footer() {
   return (
-    <Paper>Website by Ong Cheng Geng | Created with React and Material-Ui</Paper>      
+    <Paper >
+      <Typography align="center">
+        Website by Ong Cheng Geng | Created with React and Material-Ui
+      </Typography>  
+    </Paper>      
   );
 };
